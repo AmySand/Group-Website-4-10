475660b858f457e06ab3b75900a946ce0094080a
Amy Sand <amy.sand@midlandu.edu>
2019-04-10 15:34:25 -0500

    js updates

--- fox project.js
+++ fox project.js
@@ -1,19 +1,44 @@
-//Call the fox API
-
-var getFoxImg = document.getElementById("img-fluid");
-getFoxImg.innerHTML = '<img class="img-fluid" src="http:\/\/randomfox.ca\/images\/114.jpg">'
-
 function foxPhotos(){
-       var randomFox = ("http:\/\/randomfox.ca\/?i=114")
-   fetch(randomFox)
-       .then(function(response){
-               console.log(response);
-            if(!response.ok){
-                  console.log(response.status)
-          }
-            return response.json();
-        })
-       .then(function(res){
-           console.log(res);
-      })
-     };
+           var randomFox = ("http:\/\/randomfox.ca\/?i=114")
+       fetch(randomFox)
+           .then(function(response){
+                   console.log(response);
+                if(!response.ok){
+                      console.log(response.status)
+              }
+                return response.json();
+            })
+           .then(function(res){
+               console.log(res);
+          })
+         }
+
+
+window.onload = photo(){
+    var img = new Image();
+    img.src = "http:\/\/randomfox.ca\/images\/114.jpg";
+
+    img.onload = foxPhotos () {
+        // CREATE CANVAS CONTEXT.
+        var getFoxImg = document.getElementById("img-fluid");
+
+     }
+  }
+
+// var getFoxImg = document.getElementById("img-fluid");
+// getFoxImg.innerHTML = '<img class="img-fluid" src="http:\/\/randomfox.ca\/images\/114.jpg">';
+
+// function foxPhotos(){
+//        var randomFox = ("http:\/\/randomfox.ca\/?i=114")
+//    fetch(randomFox)
+//        .then(function(response){
+//                console.log(response);
+//             if(!response.ok){
+//                   console.log(response.status)
+//           }
+//             return response.json();
+//         })
+//        .then(function(res){
+//            console.log(res);
+//       })
+//      };
