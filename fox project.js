21809f3006b3de003da895949d3a8f99a1104b07
Amy Sand <amy.sand@midlandu.edu>
2019-04-10 14:45:02 -0500

    js updates

--- fox project.js
+++ fox project.js
@@ -2,8 +2,9 @@
 
 var getFoxImg = document.getElementById("img-fluid");
 
-   var foxPhotos = function foxPhotos(){
-       fetch({"image":"http:\/\/randomfox.ca\/images\/114.jpg","link":"http:\/\/randomfox.ca\/?i=114"} )
+function foxPhotos(){
+       var randomFox = {"image":"http:\/\/randomfox.ca\/images\/114.jpg","link":"http:\/\/randomfox.ca\/?i=114"} 
+   fetch(randomFox)
        .then(function(response){
                console.log(response);
             if(!response.ok){
@@ -14,36 +15,4 @@ var getFoxImg = document.getElementById("img-fluid");
        .then(function(res){
            console.log(res);
       })
-     }
-    
-
-// // window.onload
-// window.onload = function() {
-//     addListItem("window onload");
-// };
-
-// // document.getElementById('getF').addEventListener("click", function(){
-// //     fetch('https://jsonplaceholder.typicode.com/posts/1')
-//     .then(function(response){
-//         console.log(response);
-//         if(!response.ok){
-//             console.log(response.status)
-//         }
-//         return response.json();
-//     })
-//     .then(function(res){
-//         console.log(res);
-//     })
-// });
-
-//     .then(function(response){
-//         console.log(response);
-//         if(!response.ok){
-//             console.log(response.status)
-//         }
-//         return response.json();
-//     })
-//     .then(function(res){
-//         console.log(res);
-//     })
-// });
+     };
