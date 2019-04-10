caecedacb28a8975e446ae59de4fbe2bf5aede0f
ninja <ebecker03@msn.com>
2019-04-10 16:31:35 -0500

    update with javascript in the form.html just because I was testing this

--- fox project.js
+++ fox project.js
@@ -1,19 +1,33 @@
 //Call the fox API
 
-var getFoxImg = document.getElementById("img-fluid");
-getFoxImg.innerHTML = '<img class="img-fluid" src="http:\/\/randomfox.ca\/images\/114.jpg">'
+// var getFoxImg = document.getElementById("img-fluid");
+// getFoxImg.innerHTML = '<img class="img-fluid" src="http:\/\/randomfox.ca\/images\/114.jpg">'
 
-function foxPhotos(){
-       var randomFox = ("http:\/\/randomfox.ca\/?i=114")
-   fetch(randomFox)
-       .then(function(response){
-               console.log(response);
-            if(!response.ok){
-                  console.log(response.status)
-          }
-            return response.json();
-        })
-       .then(function(res){
-           console.log(res);
-      })
-     };
+// function foxPhotos(){
+//        var randomFox = ("http:\/\/randomfox.ca\/?i=114")
+//    fetch(randomFox)
+//        .then(function(response){
+//                console.log(response);
+//             if(!response.ok){
+//                   console.log(response.status)
+//           }
+//             return response.json();
+//         })
+//        .then(function(res){
+//            console.log(res);
+//       })
+//      };
+
+document.getElementById('searchbtn').addEventListener("click", function(){
+    fetch("http:\/\/randomfox.ca\/?i=53")
+    .then(function(response){
+        console.log(response);
+        if(!response.ok){
+            console.log(response.status)
+        }
+        return response.json();
+    })
+    .then(function(res){
+        console.log(res);
+    })
+});
