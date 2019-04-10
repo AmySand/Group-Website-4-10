a073d69209c8416f918ce359ea6509acd7dc8f1b
Amy Sand <amy.sand@midlandu.edu>
2019-04-10 16:31:10 -0500

    FOX

--- fox project.js
+++ fox project.js
@@ -1,55 +1,5 @@
-var foxPhoto = "";
-var imgTag = "";
-function foxAPI(){
-           var randomFox = ("http:\/\/randomfox.ca\/?i=114")
-       fetch(randomFox)
-           .then(function(response){
-                   console.log(response);
-                if(!response.ok){
-                      console.log(response.status)
-              }
-                return response.json();
-            })
-           .then(function(res){
-               console.log(res);
-               foxSlideShow = document.getElementById("fox-slideshow");
-            //    imgTag = document.getElementByClassName("img-fluid");
-                foxSlideShow.append(randomFox); 
+var randomNumber =  Math.floor(Math.random()*122) + 1;
 
-            //    fox-slideshow
-          })
-         }
-
-
-// window.onload = photo(){
-//     var img = new Image();
-//     img.src = "http:\/\/randomfox.ca\/images\/114.jpg";
-
-//     img.onload = foxAPI () {
-//         // CREATE CANVAS CONTEXT.
-//         var getFoxImg = document.getElementById("img-fluid");
-
-
-//      }
-//   }
-
-//   var getFoxImg = document.getElementById("img-fluid");
-//   getFoxImg.innerHTML = 
-
-// var getFoxImg = document.getElementById("img-fluid");
-// getFoxImg.innerHTML = '<img class="img-fluid" src="http:\/\/randomfox.ca\/images\/114.jpg">';
-
-// function foxPhotos(){
-//        var randomFox = ("http:\/\/randomfox.ca\/?i=114")
-//    fetch(randomFox)
-//        .then(function(response){
-//                console.log(response);
-//             if(!response.ok){
-//                   console.log(response.status)
-//           }
-//             return response.json();
-//         })
-//        .then(function(res){
-//            console.log(res);
-//       })
-//      };
+            //    foxSlideShow = document.getElementById("fox-slideshow");
+               var img = document.getElementsByClassName("img-fluid")[0];
+                img.src = `https://randomfox.ca/images/${randomNumber}.jpg`;
