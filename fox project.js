b489f68eff5eeb9cc8069d5682877584019824d0
Amy Sand <amy.sand@midlandu.edu>
2019-04-10 15:52:00 -0500

    update js

--- fox project.js
+++ fox project.js
@@ -1,4 +1,6 @@
-function foxPhotos(){
+var foxPhoto = "";
+var imgTag = "";
+function foxAPI(){
            var randomFox = ("http:\/\/randomfox.ca\/?i=114")
        fetch(randomFox)
            .then(function(response){
@@ -10,20 +12,29 @@ function foxPhotos(){
             })
            .then(function(res){
                console.log(res);
+               foxSlideShow = document.getElementById("fox-slideshow");
+            //    imgTag = document.getElementByClassName("img-fluid");
+                foxSlideShow.append(randomFox); 
+
+            //    fox-slideshow
           })
          }
 
 
-window.onload = photo(){
-    var img = new Image();
-    img.src = "http:\/\/randomfox.ca\/images\/114.jpg";
+// window.onload = photo(){
+//     var img = new Image();
+//     img.src = "http:\/\/randomfox.ca\/images\/114.jpg";
 
-    img.onload = foxPhotos () {
-        // CREATE CANVAS CONTEXT.
-        var getFoxImg = document.getElementById("img-fluid");
+//     img.onload = foxAPI () {
+//         // CREATE CANVAS CONTEXT.
+//         var getFoxImg = document.getElementById("img-fluid");
 
-     }
-  }
+
+//      }
+//   }
+
+//   var getFoxImg = document.getElementById("img-fluid");
+//   getFoxImg.innerHTML = 
 
 // var getFoxImg = document.getElementById("img-fluid");
 // getFoxImg.innerHTML = '<img class="img-fluid" src="http:\/\/randomfox.ca\/images\/114.jpg">';
