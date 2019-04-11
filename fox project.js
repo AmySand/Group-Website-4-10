36ad03a0adc947720a51400ab29a9e160d1d31ef
ninja <ebecker03@msn.com>
2019-04-10 21:51:22 -0500

    added container for new divs to foxproject.html and updated javascript to read API and deliver the results for ticker search to the DOM.

--- fox project.js
+++ fox project.js
@@ -1,39 +1,45 @@
-var randomNumber =  Math.floor(Math.random()*122) + 1;
+var randomNumber = Math.floor(Math.random() * 122) + 1;
 
-<<<<<<< HEAD
-// var getFoxImg = document.getElementById("img-fluid");
-// getFoxImg.innerHTML = '<img class="img-fluid" src="http:\/\/randomfox.ca\/images\/114.jpg">'
+//    foxSlideShow = document.getElementById("fox-slideshow");
+var img = document.getElementsByClassName("img-fluid")[0];
+img.src = `https://randomfox.ca/images/${randomNumber}.jpg`;
 
-// function foxPhotos(){
-//        var randomFox = ("http:\/\/randomfox.ca\/?i=114")
-//    fetch(randomFox)
-//        .then(function(response){
-//                console.log(response);
-//             if(!response.ok){
-//                   console.log(response.status)
-//           }
-//             return response.json();
-//         })
-//        .then(function(res){
-//            console.log(res);
-//       })
-//      };
 
-document.getElementById('searchbtn').addEventListener("click", function(){
-    fetch("http:\/\/randomfox.ca\/?i=53")
-    .then(function(response){
-        console.log(response);
-        if(!response.ok){
-            console.log(response.status)
-        }
-        return response.json();
-    })
-    .then(function(res){
-        console.log(res);
-    })
-});
-=======
-            //    foxSlideShow = document.getElementById("fox-slideshow");
-               var img = document.getElementsByClassName("img-fluid")[0];
-                img.src = `https://randomfox.ca/images/${randomNumber}.jpg`;
->>>>>>> 31cc6e32fd8d9feb31a3d19d993e4b74c8591793
+//Api call to stocks
+var tick = document.getElementById("ticker-symbol");
+document.getElementById("searchBtn").addEventListener("click", function (e) {
+    e.preventDefault();
+    fetch("https://financialmodelingprep.com/api/stock/list/all?datatype=json")
+        .then(function (response) {
+            console.log(response);
+            if (!response.ok) {
+                console.log(response.status)
+            }
+            return response.json();
+        })
+        .then(function (res) {
+            console.log(res);
+            var ticker = tick.value.toUpperCase();
+            // console.log(ticker);
+
+            for(i=0; i<=res.length; i++){
+                var match = res[i].Ticker;
+                // console.log(res[i].Ticker)
+                if(match === ticker){
+                   console.log(match)
+                   match = res[i];
+                //    console.log(match);
+                   var newDivTick = document.createElement("div");
+                   var newDivComp = document.createElement("div");
+                   var newDivPrice = document.createElement("div");
+                   newDivTick.innerHTML =`Ticker: ${match.Ticker}`;
+                   newDivComp.innerHTML = `Company Name: ${match.companyName}`;  
+                   newDivPrice.innerHTML = `Current Price: $${match.Price}`;
+                   document.getElementById("tickerContainer").append(newDivTick);
+                   document.getElementById("tickerContainer").append(newDivComp);
+                   document.getElementById("tickerContainer").append(newDivPrice);
+
+                } 
+            }
+            })
+        })
