a178e66624a55e39482cacb47e3d69758bef501b
Amy Sand <amy.sand@midlandu.edu>
2019-04-10 20:50:32 -0500

    speech bubble updates!

--- fox project.js
+++ fox project.js
@@ -1,4 +1,35 @@
-var randomNumber =  Math.floor(Math.random()*122);
+
+function randomFox() {
+    var randomNumber = Math.floor(Math.random() * 122);
+    var img = document.querySelector(".img-fluid")[0];
+    img.src = `https://randomfox.ca/images/${randomNumber}.jpg`;
+}
+console.log(randomFox);
+
+
+function createSpeechBubble(){
+    var newDiv = document.createElement("div");
+    console.log(newDiv);
+    newDiv.classList.add("talkbubble");
+    var tickerInput = document.querySelector("#ticker-symbol").value;
+    var companyNameInput = document.querySelector("#company-name").value;
+    newDiv.innerHTML = "The fox says your stock is " + tickerInput + "<br>" + companyNameInput;
+    document.body.appendChild(newDiv);
+}
+console.log(createSpeechBubble);
+
+document.getElementById('search').addEventListener("click", function (e) {
+    e.preventDefault();
+    createSpeechBubble();
+    console.log(createSpeechBubble);
+    // randomFox();
+    console.log(randomFox);
+})
+
+
+
+
+
 
 
 // var getFoxImg = document.getElementById("img-fluid");
@@ -6,21 +37,21 @@ var randomNumber =  Math.floor(Math.random()*122);
 
 
 
-document.getElementById('searchbtn').addEventListener("click", function(){
-    fetch("http:\/\/randomfox.ca\/?i=53")
-    .then(function(response){
-        console.log(response);
-        if(!response.ok){
-            console.log(response.status)
-        }
-        return response.json();
-    })
-    .then(function(res){
-        console.log(res);
-    })
-});
+// document.getElementById('searchbtn').addEventListener("click", function(){
+//     fetch("http:\/\/randomfox.ca\/?i=53")
+//     .then(function(response){
+//         console.log(response);
+//         if(!response.ok){
+//             console.log(response.status)
+//         }
+//         return response.json();
+//     })
+//     .then(function(res){
+//         console.log(res);
+//     })
+// });
 
-<<<<<<< HEAD
+
 // var getFoxImg = document.getElementById("img-fluid");
 // getFoxImg.innerHTML = '<img class="img-fluid" src="http:\/\/randomfox.ca\/images\/114.jpg">'
 
@@ -39,36 +70,17 @@ document.getElementById('searchbtn').addEventListener("click", function(){
 //       })
 //      };
 
-document.getElementById('searchbtn').addEventListener("click", function(){
-    fetch("http:\/\/randomfox.ca\/?i=53")
-    .then(function(response){
-        console.log(response);
-        if(!response.ok){
-            console.log(response.status)
-        }
-        return response.json();
-    })
-    .then(function(res){
-        console.log(res);
-    })
-});
-=======
-            //    foxSlideShow = document.getElementById("fox-slideshow");
-               var img = document.getElementsByClassName("img-fluid")[0];
-                img.src = `https://randomfox.ca/images/${randomNumber}.jpg`;
-<<<<<<< HEAD
+// document.getElementById('searchbtn').addEventListener("click", function(){
+//     fetch("http:\/\/randomfox.ca\/?i=53")
+//     .then(function(response){
+//         console.log(response);
+//         if(!response.ok){
+//             console.log(response.status)
+//         }
+//         return response.json();
+//     })
+//     .then(function(res){
+//         console.log(res);
+//     })
+// });
 
-
-            
-            function createSpeechBubble() {
-               var body = document.getElementById("body");
-               var speechDiv = document.createElement("div"); 
-               var bubbleText = document.createTextNode("search results go here");
-               body.append(speechDiv); 
-               speechDiv.append(bubbleText);
-            }
-
-            document.getElementsByTagName("H1")[0].setAttribute("id", "democlass");
-=======
->>>>>>> 31cc6e32fd8d9feb31a3d19d993e4b74c8591793
->>>>>>> 02c749d4644ed1e29ca3dad6c6866a5a04d83467
